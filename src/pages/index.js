b2d5ef516fd275bcb59c4d8e317b8e5430bdb146
MoonSupport <jiwon3346@naver.com>
2020-09-20 15:34:06 +0900

    setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
+import AppFrame from "../components/Layout/AppFrame"
+import SearchBar from "../components/Forms/SearchBar"
+import RecommandCard from "../components/Lists/RecommandCard/index"
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles(() => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}))
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const classes = useStyles()
+  return (
+    <AppFrame>
+      <div className={classes.container}>
+        <SearchBar />
+        <RecommandCard />
+      </div>
+    </AppFrame>
+  )
 }
