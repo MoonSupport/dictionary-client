63f6d23e8d1753d6f5d55f1de51ef56db56121bd
MoonSupport <jiwon3346@naver.com>
2020-09-20 23:52:40 +0900

    _app

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+function MyApp({ Component, pageProps }) {
+
+  return (
+      <>
+      <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet" />
+      <Component {...pageProps} />
+      </>
+  )
+}
+
+MyApp.getInitialProps = async appContext => {
+  const appProps = await App.getInitialProps(appContext)
+  return { ...appProps }
+}
+
+export default MyApp
