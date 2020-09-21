3dc03869af513fd23c6bc45c0c3d678d4de18174
MoonSupport <jiwon3346@naver.com>
2020-09-21 22:58:05 +0900

    add index

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,39 +1,53 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-
-
+import { Paper, Typography, Box, makeStyles } from "@material-ui/core"
 
 const useStyles = makeStyles({
-    MainTitle : {
-        width: '90vw',
-        height:'100vh',
-        backgroundRepeat: 'repeat',
-    }
-  })
+    main: {
+        borderBottom:'5px solid #CDE8EF',
+        display: 'inline-block',
+        marginBottom : '100px'
+    },
+    chapterContianer: {
+        display: 'flex',
+        marginBottom: '50px'
+    },
+    chapterTitle1: {
+        display: 'inline-block',
+        padding: '0.5rem 1rem',
+        backgroundColor: '#CDE8EF'
+    },
 
-const MainTitle = () => {
+    chapterTitle2: {
+        display: 'inline-block',
+        padding: '0.5rem 1rem',
+    }
+})
+
+const IndexList = () => {
+    const classes = useStyles()
+    return (
+        <Box>
+        <Box className={classes.chapterContianer}>
+            <Typography className={classes.chapterTitle1}  variant="h3" color="secondary">FrontEnd</Typography>
+            <Typography className={classes.chapterTitle2} variant="h3" color="primary">프론트 엔드</Typography>
+        </Box>
+        <Box>
+            <Typography>단어</Typography>
+        </Box>
+        </Box>
+        )
+}
+
+const Index = () => {
 const classes = useStyles()
 return    (
-    <Box className={classes.MainTitle}>
-        <Typography component='h1'>
-            DICTIONARY
-        </Typography>
-        <Typography component='h1'>
-            사전
-        </Typography>
-        <Typography component='h1'>
-            事前
-        </Typography>
-        <Typography component='h1'>
-        dicionário
-        </Typography>        <Typography component='h1'>
-        অভিধান
-        </Typography>
-    </Box>
+    <Paper elevation={0}>
+        <Typography  className={classes.main} variant='h2' color='primary'>INDEX</Typography>
+        <IndexList />
+    </Paper>
 
 )
 
 }
 
-export default MainTitle
+export default Index
