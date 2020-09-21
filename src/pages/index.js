ec1af53f3be73444c41af8cc149faab9045741b2
MoonSupport <jiwon3346@naver.com>
2020-09-21 22:58:32 +0900

    meaning index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,13 @@
 import React from "react"
 import AppFrame from "../components/Layout/AppFrame"
-import SideTitle from "../components/SideTitle"
 import Index from "../components/Index"
 
-import { makeStyles } from "@material-ui/core"
 
-const useStyles = makeStyles(() => ({
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-}))
 
 export default function Home() {
-  const classes = useStyles()
   return (
     <AppFrame>
-      <div className={classes.container}>
         <Index />
-        <SideTitle />
-      </div>
     </AppFrame>
   )
 }
