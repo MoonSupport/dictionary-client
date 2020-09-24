dccfe1caed4b68fd8af9bc395d096ea5c57e954f
MoonSupport <jiwon3346@naver.com>
2020-09-24 22:51:16 +0900

    index style 수정

--- src/components/Lists/RecommandIndex/RecommandIndexForm.js
+++ src/components/Lists/RecommandIndex/RecommandIndexForm.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles"
 const useStyles = makeStyles({
     chapterContianer: {
         display: 'flex',
-        marginBottom: '50px'
+        marginBottom: 20
     },
     chapterTitle1: {
         display: 'inline-block',
@@ -16,9 +16,12 @@ const useStyles = makeStyles({
         display: 'inline-block',
         padding: '0.5rem 1rem',
     },
-    listContainer: {
+    words: {
         fontSize: 26,
         marginBottom: 10,
+    },
+    listContainer: {
+        marginBottom: 40,
     }
 })
 
@@ -30,10 +33,10 @@ export default function RecommandIndexForm({ words, engTitle, korTitle }) {
     <Typography className={classes.chapterTitle1}  variant="h3" color="secondary">{engTitle}</Typography>
     <Typography className={classes.chapterTitle2} variant="h3" color="primary">{korTitle}</Typography>
       </Box>
-      <Box>
+      <Box className={classes.listContainer}>
           {words.map(word=>(
               <Link href={word?.frontmatter?.slug}>
-              <Box className={classes.listContainer}>
+              <Box className={classes.words}>
                  {word?.frontmatter?.title + ' '}
                  [{word?.frontmatter?.label.join(',')}]
               </Box>
