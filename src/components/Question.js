61aef862ae242588427d0345f04de2d974150544
MoonSupport <jiwon3346@naver.com>
2020-10-10 17:10:27 +0900

    unnessary question re-render remove

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useEffect, useMemo} from "react"
 import { chunk, random} from 'lodash'
 // import { makeStyles } from "@material-ui/core"
 import { useStaticQuery } from 'gatsby'
@@ -9,7 +9,7 @@ import { useStaticQuery } from 'gatsby'
 
 const Question = () => {
 // const classes = useStyles()
-// const chooses = useState([])
+const [chooses, setChooses] = useState([])
 const [page, setPage] = useState(0)
 
 const {
@@ -31,10 +31,13 @@ const {
 
 
 const questionss = chunk(samples, 5)
-const questionMap = []
-const answers = []
 
-questionss.map((questions, index)=> {
+
+const [questionMap, answers] = useMemo(()=> {
+  console.log('meemo')
+  const questionMap = []
+  const answers = []
+  questionss.map((questions, index)=> {
     const answer = random(0,4)
     const question = questions[answer].html
     const [front, back] = trimWordFromBraket(questions[answer].frontmatter.title)
@@ -46,7 +49,9 @@ questionss.map((questions, index)=> {
     }
     questionMap.push(formattedQuestion)
     answers.push(answer)
-})
+  })
+  return [questionMap, answers]
+}, [])
 
 function trimWordFromBraket(title) {
   
@@ -64,27 +69,48 @@ function trimWordFromBraket(title) {
   return [front,back]
 }
 
+const handleOnClick = (e) => {
+  const answer = e.target?.innerHTML?.split('.')[0]
+  if(!answer) return
+
+  setChooses((chooses=> {
+    chooses[page] = answer
+    return [...chooses]
+  }))
+}
 
 
 return    (
     <>
     <div>Qustion</div>
     <div>
-      <div dangerouslySetInnerHTML={{ __html: questionMap[page] }}/>
+      <Problem problem={questionMap[page]} />
+      <Viewer questions={questionss[page]} handleOnClick={handleOnClick} />
+      {chooses[page]}
     </div>
-    {questionss[page].map((question, index)=> {
-            return (
-                <>
-                  <div>
-                      {index}. 
-                      {' '+question.frontmatter.title}
-                  </div>
-                </>
-            )
-    })}
+ 
     </>
 )
 
+}
+
+const Problem = ({problem}) => {
+  return <div dangerouslySetInnerHTML={{ __html: problem }}/>
+}
+
+const Viewer = ({questions, handleOnClick}) => {
+  return (<>
+  {questions.map((question, index)=> {
+    return (
+        <>
+          <div onClick={handleOnClick}>
+              {index}. 
+              {' '+question.frontmatter.title}
+          </div>
+        </>
+    )
+})}
+</>)
 }
 
 export default Question
