9842269df445270b04c38cd84fb87358f3033237
MoonSupport <jiwon3346@naver.com>
2020-10-11 16:19:50 +0900

    replace All 사용 불가로 수정

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect, useMemo } from "react"
-import { chunk, random } from "lodash"
-import { makeStyles } from "@material-ui/core"
-import { useStaticQuery } from "gatsby"
-const useStyles = makeStyles({
-  selected: e => {
-    console.log(e)
-    return "black"
-  },
-})
+import React, { useState, useMemo } from "react";
+import { chunk, random } from "lodash";
+import { useStaticQuery } from "gatsby";
 
 const Question = () => {
-  const [chooses, setChooses] = useState([])
-  const [page, setPage] = useState(0)
+  const [chooses, setChooses] = useState([]);
+  const [page, setPage] = useState(0);
 
   const {
     allMarkdownRemark: { nodes: samples },
@@ -28,57 +21,58 @@ const Question = () => {
         }
       }
     }
-  `)
+  `);
 
-  const questionss = chunk(samples, 5)
+  const questionss = chunk(samples, 5);
 
   const [questionMap, answers] = useMemo(() => {
-    const questionMap = []
-    const answers = []
-    questionss.map((questions, index) => {
-      const answer = random(0, 4)
-      const question = questions[answer].html
+    const questionMap = [];
+    const answers = [];
+    questionss.map((questions) => {
+      const answer = random(0, 4);
+      const question = questions[answer].html;
       const [front, back] = trimWordFromBraket(
         questions[answer].frontmatter.title
-      )
-      const frontRegex = new RegExp(front, "gi")
+      );
+
+      const frontRegex = new RegExp(front, "gi");
       let formattedQuestion = question
-        .replaceAll(frontRegex, "[ㅁㅁㅁ]")
-        .replaceAll(questions[answer].frontmatter.mean, "[ㅁㅁㅁ]")
+        .replace(frontRegex, "[ㅁㅁㅁ]")
+        .replace(questions[answer].frontmatter.mean, "[ㅁㅁㅁ]");
       if (back) {
-        const backRegex = new RegExp(back, "gi")
-        formattedQuestion.replaceAll(backRegex, "[ㅁㅁㅁ]")
+        const backRegex = new RegExp(back, "gi");
+        formattedQuestion.replace(backRegex, "[ㅁㅁㅁ]");
       }
-      questionMap.push(formattedQuestion)
-      answers.push(answer)
-    })
-    return [questionMap, answers]
-  }, [])
+      questionMap.push(formattedQuestion);
+      answers.push(answer);
+    });
+    return [questionMap, answers];
+  }, []);
 
   function trimWordFromBraket(title) {
     if (!title) {
-      return
+      return;
     }
-    let front = title
-    let back = ""
+    let front = title;
+    let back = "";
     if (typeof title === "string") {
       if (title.includes("(")) {
-        front = title.split("(")[0]
-        back = title.split("(")[1].replace(")", "")
+        front = title.split("(")[0];
+        back = title.split("(")[1].replace(")", "");
       }
     }
-    return [front, back]
+    return [front, back];
   }
 
-  const handleOnClick = e => {
-    const answer = e.target?.innerHTML?.split(".")[0]
-    if (!answer) return
+  const handleOnClick = (e) => {
+    const answer = e.target?.innerHTML?.split(".")[0];
+    if (!answer) return;
 
-    setChooses(chooses => {
-      chooses[page] = answer
-      return [...chooses]
-    })
-  }
+    setChooses((chooses) => {
+      chooses[page] = answer;
+      return [...chooses];
+    });
+  };
 
   return (
     <>
@@ -93,43 +87,42 @@ const Question = () => {
         <NavigationButton setPage={setPage} />
       </div>
     </>
-  )
-}
+  );
+};
 
 const NavigationButton = ({ setPage }) => {
   const handlePreviousClick = () => {
-    setPage(page => {
+    setPage((page) => {
       if (page <= 0) {
-        alert("첫 페이지 입니다.")
-        return
+        alert("첫 페이지 입니다.");
+        return;
       }
-      return --page
-    })
-  }
+      return --page;
+    });
+  };
 
   const handleNextClick = () => {
-    setPage(page => {
+    setPage((page) => {
       if (page >= 9) {
-        alert("마지막 페이지입니다.")
-        return
+        alert("마지막 페이지입니다.");
+        return;
       }
-      return ++page
-    })
-  }
+      return ++page;
+    });
+  };
   return (
     <>
       <div onClick={handlePreviousClick}>previous</div>
       <div onClick={handleNextClick}>next</div>
     </>
-  )
-}
+  );
+};
 
 const Problem = ({ problem }) => {
-  return <div dangerouslySetInnerHTML={{ __html: problem }} />
-}
+  return <div dangerouslySetInnerHTML={{ __html: problem }} />;
+};
 
 const Viewer = ({ questions, handleOnClick, chooses }) => {
-  const classes = useStyles()
   return (
     <>
       {questions.map((question, index) => {
@@ -138,15 +131,14 @@ const Viewer = ({ questions, handleOnClick, chooses }) => {
             <div
               style={chooses == index ? { color: "green" } : { color: "black" }}
               onClick={handleOnClick}
-              className={classes.selected}
             >
               {index}.{" " + question.frontmatter.title}
             </div>
           </>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
