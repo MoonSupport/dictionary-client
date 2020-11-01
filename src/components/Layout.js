fa052e7d41aebe8ec6c7093c7e4b6c6732087b6e
MoonSupport <jiwon3346@naver.com>
2020-11-01 22:12:35 +0900

    layout 수정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,13 @@ import React from "react"
 import { makeStyles,  Box } from "@material-ui/core"
 const useStyles = makeStyles({
   layout: {
+    width: 'calc(100% - 40px)',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
     flexFlow: 'column nowrap',
-    margin: '0px 80px',
+    margin: '0px 20px',
     height: '100vh'
   },
 })
